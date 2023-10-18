501c0447b83721fbf6d6be2f3764eb5b1a7fe26e
ismaildev49 <ismaelbentatou@gmail.com>
2023-10-18 18:46:01 +0200

    fin

--- app.js
+++ app.js
@@ -11,49 +11,54 @@ let dialog = document.querySelector("dialog")
 
 const searchMeal = async (val) => {
     const res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${val}`)
+    
     const data = await res.json()
     result.innerHTML = ""
-    data.meals.forEach((e)=>{
-        let newDiv = document.createElement("div")
-        newDiv.classList.add("results")
-        newDiv.innerHTML=`<h2 class="title">${e.strMeal}</h2>
-        <div class="image">
-            <img src="${e.strMealThumb}" alt="">
-        </div>
-        `
-        result.appendChild(newDiv)
-
-        newDiv.addEventListener("click", ()=>{
-            const ingredients= []
-            for (i = 1 ; i<=20; i++){
-                if (e[`strIngredient${i}`]) {
-                    ingredients.push(`${e[`strIngredient${i}`]} - ${e[`strMeasure${i}`]}`)
-                }
-            }
-            console.log(ingredients);
-            
-            dialog.innerHTML = `<div class="dialog-container">
-            <span class = "cross">x</span>
-            <div class="modal-image">
-              <img src="${e.strMealThumb}" alt="" />
+    if (data.meals) {
+        data.meals.forEach((e)=>{
+            let newDiv = document.createElement("div")
+            newDiv.classList.add("results")
+            newDiv.innerHTML=`<h2 class="title">${e.strMeal}</h2>
+            <div class="image">
+                <img src="${e.strMealThumb}" alt="">
             </div>
-            <h2 class="modal-title">${e.strMeal}</h2>
-            <p class="ingredients"><h3>Ingredients</h3>${ingredients.join(" ")}</p>
-            <p class="cooking"><h3>Instructions</h3>${e.strInstructions}</p>
-          </div>`
-            dialog.showModal()
-            const cross = document.querySelector(".cross")
-            cross.addEventListener("click", () => {
-                dialog.close()
-            })
-            document.addEventListener("click", (e) =>{
-                e.stopPropagation()
-                if(e.target.innerHTML.includes("dialog-container")){
+            `
+            result.appendChild(newDiv)
+    
+            newDiv.addEventListener("click", ()=>{
+                const ingredients= []
+                for (i = 1 ; i<=20; i++){
+                    if (e[`strIngredient${i}`]) {
+                        ingredients.push(`${e[`strIngredient${i}`]} - ${e[`strMeasure${i}`]}`)
+                    }
+                }
+                
+                dialog.innerHTML = `<div class="dialog-container">
+                <span class = "cross">x</span>
+                <div class="modal-image">
+                  <img src="${e.strMealThumb}" alt="" />
+                </div>
+                <h2 class="modal-title">${e.strMeal}</h2>
+                <p class="ingredients"><h3>Ingredients</h3>${ingredients.join(" ")}</p>
+                <p class="cooking"><h3>Instructions</h3>${e.strInstructions}</p>
+              </div>`
+                dialog.showModal()
+                const cross = document.querySelector(".cross")
+                cross.addEventListener("click", () => {
                     dialog.close()
-                };
+                })
+                document.addEventListener("click", (e) =>{
+                    e.stopPropagation()
+                    if(e.target.innerHTML.includes("dialog-container")){
+                        dialog.close()
+                    };
+                })
             })
         })
-    })
+    } else {
+        alert("There is no meal with this name")
+    }
+    
     
     
     // return data.meals
@@ -63,7 +68,12 @@ form.addEventListener("submit", (e)=> {
     e.preventDefault()
     const inputValue = input.value.trim()
     input.value = ""
-    searchMeal(inputValue)
+    if (!inputValue.length) {
+        alert("Please enter something")
+    } else {
+        searchMeal(inputValue)
+    }
+    
     // console.log(arrWithMeals, typeof arrWithMeals);
     // console.log(arrWithMeals);
 
